8c60e6aa84e22acfab3453a243e697e2222627de
Karthikeyan-Appu <85619598+Karthikeyan-Appu@users.noreply.github.com>
2024-04-04 21:23:07 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,32 @@
-from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+from skimage.metrics import structural_similarity as ssim
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "https://pneumo-ai.vercel.app"}})
-
 model = load_model("trained.h5")
-classes = ['Normal', 'Pneumonia']  # Define the classes for prediction
+classes = ['Normal', 'Pneumonia']
+reference_image = cv2.imread('reference_image.jpg')  # Load your reference image here
 
 def preprocess_image(image):
     img = cv2.resize(image, (300, 300))
     img = img / 255.0
     img = img.reshape(1, 300, 300, 3)
     return img
+
+def compare_images(image):
+    preprocessed_image = preprocess_image(image)
+
+    # Compare the uploaded image with the reference image using SSI
+    ssi_index, _ = ssim(image, reference_image, full=True)
+    
+    # Perform prediction only if SSI index is above a certain threshold
+    if ssi_index > 0.7:
+        prediction = model.predict(preprocessed_image)[0][0]
+        predicted_class = classes[int(prediction >= 0.5)]
+        return predicted_class, ssi_index
+    else:
+        return "Comparison Failed", ssi_index
 
 @app.route("/")
 def index():
@@ -30,10 +42,8 @@ def predict():
         return jsonify({'error': 'No selected file'}), 400
 
     image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
-    preprocessed_image = preprocess_image(image)
-    prediction = model.predict(preprocessed_image)[0][0]
-    predicted_class = classes[int(prediction >= 0.5)]
-    return jsonify({"prediction": predicted_class})
+    predicted_class, ssi_index = compare_images(image)
+    return jsonify({"prediction": predicted_class, "ssi_index": ssi_index})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
